fc00cf4262983568b077492ce60c48c6dd41bb96
Ovidiu Panait <ovidiu.panait@windriver.com>
2021-11-25 20:14:55 +0200

    oeqa: syzkaller: use qemu provided by qemu-system-native
    Signed-off-by: Ovidiu Panait <ovidiu.panait@windriver.com>

--- lib/oeqa/selftest/cases/syzkaller.py
+++ lib/oeqa/selftest/cases/syzkaller.py
@@ -8,11 +8,11 @@ from oeqa.utils.commands import runCmd, bitbake, get_bb_var, get_bb_vars
 class TestSyzkaller(OESelftestTestCase):
     def setUpSyzkallerConfig(self):
         syz_target_sysroot = get_bb_var('PKGD', 'syzkaller')
-
         syz_target = os.path.join(syz_target_sysroot, 'usr')
         syz_workdir = os.path.join(self.topdir, 'syz_workdir')
         syz_cfg = os.path.join(syz_workdir, 'syzkaller.cfg')
 
+        qemu_native_bin = os.path.join(self.qemu_native_sysroot, 'usr/bin/qemu-system-x86_64')
         kernel_cmdline = "rootfs=/dev/sda dummy_hcd.num=%s" % (self.dummy_hcd_num)
         kernel_objdir = self.deploy_dir_image
 
@@ -39,6 +39,7 @@ class TestSyzkaller(OESelftestTestCase):
 		"cmdline": "%s",
 		"cpu": %s,
 		"mem": %s,
+		"qemu": "%s",
 		"qemu_args": "-device virtio-scsi-pci,id=scsi -device scsi-hd,drive=rootfs -enable-kvm -cpu host,migratable=off",
 		"image_device": "drive index=0,id=rootfs,if=none,media=disk,file="
 	}
@@ -46,7 +47,7 @@ class TestSyzkaller(OESelftestTestCase):
 """
 % (syz_workdir, kernel_objdir, self.kernel_src, self.rootfs, syz_target, \
    self.syz_qemu_vms, self.kernel, kernel_cmdline, self.syz_qemu_cpus, \
-   self.syz_qemu_mem))
+   self.syz_qemu_mem, qemu_native_bin))
 
         self.syz_cfg = syz_cfg
 
@@ -89,13 +90,15 @@ IMAGE_ROOTFS_EXTRA_SPACE = "64000"
         self.kernel = os.path.join(self.deploy_dir_image, 'bzImage')
         self.rootfs = os.path.join(self.deploy_dir_image, '%s-%s.%s' % (self.image, self.machine, self.fstype))
 
-        self.setUpSyzkallerConfig()
-
         bitbake(self.image, output_log=self.logger)
-        bitbake('syzkaller-native -c addto_recipe_sysroot', output_log=self.logger)
         bitbake('syzkaller', output_log=self.logger)
+        bitbake('syzkaller-native -c addto_recipe_sysroot', output_log=self.logger)
+        bitbake('qemu-system-native -c addto_recipe_sysroot', output_log=self.logger)
 
         self.syz_native_sysroot = get_bb_var('RECIPE_SYSROOT_NATIVE', 'syzkaller-native')
+        self.qemu_native_sysroot = get_bb_var('RECIPE_SYSROOT_NATIVE', 'qemu-system-native')
+
+        self.setUpSyzkallerConfig()
 
     def test_syzkaller(self):
         runCmd(['syz-manager', '-config', self.syz_cfg], native_sysroot = self.syz_native_sysroot, timeout=self.syz_fuzztime, output_log=self.logger, ignore_status=True, shell=False)
